fb798797117262e8226b08dc917845a88af0f2ce
satchapum <satchapum.mail@mail.kmutt.ac.th>
2023-09-19 16:48:41 +0700

    FIX: game.js can reset

--- Week 7/assignment7/assignment7/game.js
+++ Week 7/assignment7/assignment7/game.js
@@ -2,27 +2,32 @@ window.onload = pageLoad;
 
 function pageLoad(){
 	document.getElementById("start").onclick = startGame;
+	var x = document.getElementById('clock');
+	x.innerHTML = 30;
 }
 
+var timer = null;
+
 function startGame(){
+	clearInterval(timer);
+	clearScreen();
 	alert("Ready");
 	addBox();
 	timeStart();
 }
 
 function timeStart(){
-	var allbox = document.querySelectorAll("#layer div");
-		
+	
 	var TIMER_TICK = 1000;
-	var timer = null;
-	var min = 0.5; // 0.5 minute
+	var min = 0.5;
 	var second = min*60; 
 	var x = document.getElementById('clock');
-	//setting timer using setInterval function
+	console.log(timer);
 	timer = setInterval(timeCount,TIMER_TICK);
+	
 	function timeCount(){
-
 		var allbox = document.querySelectorAll("#layer div");
+
 		second--;
 		x.innerHTML = second;
 		console.log(allbox.length)
@@ -30,22 +35,20 @@ function timeStart(){
 		if(allbox.length === 0){
 			alert("Wingame");
 			clearScreen();
+			x.innerHTML = 30;
 			clearInterval(timer);
 		}
 		else if(second < 0){
 			alert("Lose");
 			clearScreen();
+			x.innerHTML = 30;
 			clearInterval(timer);
 		}
 	}
-		// จัดการเกี่ยวกับเวลา เช่น ถ้ายังมีกล่องเหลืออยู่ เวลาจะลดลงเรื่อยๆ 
-		// ถ้าไม่มีกล่องเหลือแล้ว และเวลายังเหลืออยู่จะขึ้นว่า You win!
-		// ถ้าเวลาหมด แต่ยังมีกล่องเหลืออยู่ จะบอกว่า Game over และทำsการ clear screen
 }
 
 
 function addBox(){
-	// สร้างกล่องตาม input ที่เราใส่ 
 	var numbox = document.getElementById("numbox").value;
 	var gameLayer = document.getElementById("layer");
 	console.log(gameLayer);
@@ -57,23 +60,18 @@ function addBox(){
 		tempbox.style.left = Math.random() * (500 - 25) + "px";
 		tempbox.style.top = Math.random() * (500 - 25) + "px";
 		tempbox.style.backgroundColor = colorDrop;
-		//add element to HTML node
 		gameLayer.appendChild(tempbox);
 		bindBox(tempbox);
 	}
 }
 
 function bindBox(box){
-	//เมื่อกดแล้ว กล่องจะหายไป
 	box.onclick = box.remove;
-	
 }
 
 function clearScreen(){
-	// ทำการลบ node ของกล่องทั้งหมด ออกจาก หน้าจอ
 	var allbox = 0;
 	var allbox = document.querySelectorAll("#layer div");
-	//delete all  div
 	for (var i=0;i<allbox.length;i++){
 		allbox[i].remove();
 	}
