f1d8d66b6bc35ddee5f5b9a577725e63a4b561fe
satchapum <satchapum.mail@mail.kmutt.ac.th>
2023-09-19 12:47:17 +0700

    week7

--- Week 7/assignment7/assignment7/game.js
+++ Week 7/assignment7/assignment7/game.js
@@ -1,8 +1,7 @@
 window.onload = pageLoad;
 
 function pageLoad(){
-	
-
+	document.getElementById("start").onclick = startGame;
 }
 
 function startGame(){
@@ -12,53 +11,71 @@ function startGame(){
 }
 
 function timeStart(){
+	var allbox = document.querySelectorAll("#layer div");
+		
 	var TIMER_TICK = 1000;
 	var timer = null;
 	var min = 0.5; // 0.5 minute
 	var second = min*60; 
 	var x = document.getElementById('clock');
 	//setting timer using setInterval function
-	timer = ;
-	
+	timer = setInterval(timeCount,TIMER_TICK);
 	function timeCount(){
+
 		var allbox = document.querySelectorAll("#layer div");
+		second--;
+		x.innerHTML = second;
+		console.log(allbox.length)
+
+		if(allbox.length === 0){
+			alert("Wingame");
+			clearScreen();
+			clearInterval(timer);
+		}
+		else if(second < 0){
+			alert("Lose");
+			clearScreen();
+			clearInterval(timer);
+		}
+	}
 		// จัดการเกี่ยวกับเวลา เช่น ถ้ายังมีกล่องเหลืออยู่ เวลาจะลดลงเรื่อยๆ 
 		// ถ้าไม่มีกล่องเหลือแล้ว และเวลายังเหลืออยู่จะขึ้นว่า You win!
-		// ถ้าเวลาหมด แต่ยังมีกล่องเหลืออยู่ จะบอกว่า Game over และทำการ clear screen
-	}
+		// ถ้าเวลาหมด แต่ยังมีกล่องเหลืออยู่ จะบอกว่า Game over และทำsการ clear screen
 }
+
 
 function addBox(){
 	// สร้างกล่องตาม input ที่เราใส่ 
-	var numbox = ;
-	var gameLayer = ;
-	var colorDrop = ;
+	var numbox = document.getElementById("numbox").value;
+	var gameLayer = document.getElementById("layer");
+	console.log(gameLayer);
+	var colorDrop = document.getElementById("color").value;
 	for (var i =0; i<numbox;i++){
-		var tempbox = ;
-		tempbox.className = ;
+		var tempbox = document.createElement('div');
+		tempbox.className = "square";
 		tempbox.id = "box"+i;
 		tempbox.style.left = Math.random() * (500 - 25) + "px";
 		tempbox.style.top = Math.random() * (500 - 25) + "px";
+		tempbox.style.backgroundColor = colorDrop;
 		//add element to HTML node
-		gameLayer.;
+		gameLayer.appendChild(tempbox);
 		bindBox(tempbox);
 	}
 }
 
 function bindBox(box){
 	//เมื่อกดแล้ว กล่องจะหายไป
-	box.onclick = function(){
-		
-	}
+	box.onclick = box.remove;
+	
 }
 
 function clearScreen(){
 	// ทำการลบ node ของกล่องทั้งหมด ออกจาก หน้าจอ
-	var allbox = ;
-
+	var allbox = 0;
+	var allbox = document.querySelectorAll("#layer div");
 	//delete all  div
 	for (var i=0;i<allbox.length;i++){
-		
+		allbox[i].remove();
 	}
 }
 
